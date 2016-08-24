9fc842b01e79486e020ba122e7a12402cb407950
margrink <maria.grinko.820@gmail.com>
2016-08-24 01:47:58 +0000

    update server.js for heroku deployment

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 var rules = require("./app/index.js");
 app.use(rules);
 
-var port = "8080";
-var host = "0.0.0.0"
+var port = process.env.PORT || "8080";
+var host = process.env.HOST || "0.0.0.0"
 
 app.listen(port, host);
 console.log("Listening on 0.0.0.0:8080");
