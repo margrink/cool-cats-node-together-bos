2001e71e8a88fec488714678723809a76f0fdb25
margrink <maria.grinko.820@gmail.com>
2016-08-24 01:06:43 +0000

    add server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-require("express");
+var port = "8080";
+var host = "0.0.0.0"
+var express = require("express");
+var app = express();
+app.listen(port, host);
+console.log("Listening on 0.0.0.0:8080");
